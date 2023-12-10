3ae4ef308169d0a1cdf75800002dad7ae683b98f
Shreyas Srikanth <sshreyas567@gmail.com>
2023-12-11 02:55:47 +0530

    new feature added

--- ForgotPassword/forgotpasswordscript.js
+++ ForgotPassword/forgotpasswordscript.js
@@ -1,17 +1,25 @@
 let form = document.getElementById('form');
+const token = localStorage.getItem('token');
 
-form.addEventListener('submit', async (e) =>{
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
-
     let email = document.getElementById('email').value;
+     
 
-    const response = await axios.post('http://localhost:4000/users/forgotpassword', {
+    const response = await axios.post('http://localhost:4000/password/forgotpassword', {
         email: email,
+        isactive: true
+    }, {
+        headers: {
+            'Authorization': token
+        }
     });
 
-    if(response.status === 200){
-        window.location.href = '../Login/Login.html'
+    if (response.status === 200) {
+        window.location.href = '../Login/Login.html';
     } else {
-        alert("email not registered with us");
+        alert("Email not registered with us");
     }
-})
+});
+
+
--- FrontEnd/frontendscript.js
+++ FrontEnd/frontendscript.js
@@ -42,6 +42,7 @@ prevDayButton.addEventListener('click', () => {
         currentDate.setFullYear(currentDate.getFullYear() - 1);
         displayCurrentYear();
     }
+    updateExpensesView(viewExpensesSelect.value);
 });
 
 nextDayButton.addEventListener('click', () => {
@@ -55,6 +56,7 @@ nextDayButton.addEventListener('click', () => {
         currentDate.setFullYear(currentDate.getFullYear() + 1);
         displayCurrentYear();
     }
+    updateExpensesView(viewExpensesSelect.value);
 });
 
 const categoryBar = document.getElementById('categoryBar');
@@ -78,57 +80,131 @@ viewExpensesSelect.addEventListener('change', () => {
     } else if (selectedView === 'yearly') {
         displayCurrentYear();
     }
+    updateExpensesView(selectedView);
 });
 
-async function fetchExpense(){
+async function fetchExpenseByDate(startDate, endDate) {
     try {
-        var token = localStorage.getItem('token')
-        const response = await axios.get('http://localhost:4000/expense/fetchexpense', {
-        headers: {
-            'Authorization': token
-        }
-    });
-
-    return response.data
-
-    } catch(err){
-        console.log("Failed to fetch expenses",err)
+        const token = localStorage.getItem('token');
+        const response = await axios.get(`http://localhost:4000/expense/fetchexpense?startDate=${formattedStartDate}&endDate=${formattedEndDate}`, {
+            headers: {
+                'Authorization': token
+            }
+        });
+        return response.data;
+    } catch (err) {
+        console.log("Failed to fetch expenses", err);
+        return []; // Return an empty array or handle the error according to your app logic
     }
+}
+
+function calculateDateRange(viewType) {
+    const today = new Date();
+    let startDate, endDate;
+
+    switch (viewType) {
+        case 'daily':
+            startDate = new Date(currentDate);
+            endDate = new Date(currentDate);
+            break;
+        case 'monthly':
+            startDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+            endDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+            break;
+        case 'yearly':
+            startDate = new Date(currentDate.getFullYear(), 0, 1);
+            endDate = new Date(currentDate.getFullYear(), 11, 31);
+            break;
+        default:
+            break;
+    }
+
+    return { startDate, endDate };
+}
+
+async function updateExpensesView(viewType) {
+    const { startDate, endDate } = calculateDateRange(viewType);
+    const expenses = await fetchExpenseByDate(startDate, endDate);
+    displayExpenses(expenses);
 }
 
 function displayExpenses(expenses) {
     const expenseList = document.getElementById('expenseList');
-    
+    expenseList.innerHTML = '';
+
+    if (expenses.length === 0) {
+        const noExpensesMessage = document.createElement('p');
+        noExpensesMessage.textContent = 'No expenses found for the selected period.';
+        expenseList.appendChild(noExpensesMessage);
+        return;
+    }
+
+    const expenseTable = document.createElement('table');
+    expenseTable.classList.add('expense-table');
+
+    const tableHeader = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    const headerCategories = ['Category', 'Description', 'Amount', 'Created At', 'Delete'];
+    headerCategories.forEach((category) => {
+        const headerCell = document.createElement('th');
+        headerCell.textContent = category;
+        headerRow.appendChild(headerCell);
+    });
+    tableHeader.appendChild(headerRow);
+    expenseTable.appendChild(tableHeader);
+
+    const tableBody = document.createElement('tbody');
 
     expenses.forEach((expense) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `Category: ${expense.category}, Description: ${expense.description}, Amount: ${expense.amount}, Created At: ${expense.createdAt}`;
+        const row = document.createElement('tr');
 
+        const categoryCell = document.createElement('td');
+        categoryCell.textContent = expense.category;
+        row.appendChild(categoryCell);
+
+        const descriptionCell = document.createElement('td');
+        descriptionCell.textContent = expense.description;
+        row.appendChild(descriptionCell);
+
+        const amountCell = document.createElement('td');
+        amountCell.textContent = expense.amount;
+        row.appendChild(amountCell);
+
+        const createdAtCell = document.createElement('td');
+        const date = new Date(expense.createdAt);
+        const formattedDate = `${date.getDate()} ${date.toLocaleString('default', { month: 'short' })} ${date.getFullYear()}`;
+        createdAtCell.textContent = formattedDate;
+        row.appendChild(createdAtCell);
+
+        const deleteButtonCell = document.createElement('td');
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
         deleteButton.addEventListener('click', async () => {
             const token = localStorage.getItem('token');
             const response = await axios.post('http://localhost:4000/expense/deleteexpense', {
-                    ID: expense.id,
-                    amount: expense.amount
-            },{
-                headers:{
-                    'Authorization' : token
+                ID: expense.id,
+                amount: expense.amount
+            }, {
+                headers: {
+                    'Authorization': token
                 }
             });
             if (response.status === 200) {
-                const updatedExpenses = await fetchExpense();
-                if (updatedExpenses) {
-                    expenseList.innerHTML = '';
-                    displayExpenses(updatedExpenses);
-                }
+                updateExpensesView(viewExpensesSelect.value);
             }
         });
+        deleteButtonCell.appendChild(deleteButton);
+        row.appendChild(deleteButtonCell);
 
-        listItem.appendChild(deleteButton);
-        expenseList.appendChild(listItem);
+        tableBody.appendChild(row);
     });
+
+    expenseTable.appendChild(tableBody);
+    expenseList.appendChild(expenseTable);
 }
+
+updateExpensesView(viewExpensesSelect.value);
+
 
     fetchExpense()
     .then(res => {
